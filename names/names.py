06f1e8108021d2c03ea89fdf65942b7c15c1c340
willsn <willsn@gmail.com>
2020-07-17 10:39:39 -0600

    names done

--- names/names.py
+++ names/names.py
@@ -1,3 +1,27 @@
+#*You may not use the built in Python list, set, or dictionary in your solution for this problem. 
+#  However, you can and should use the provided `duplicates` list to return your solution.*
+#(Hint: You might try importing a data structure you built during the week)
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+    
+    def __len__(self):
+        return len(self.storage)
+        #pass
+
+    def enqueue(self, value):
+        return self.storage.insert(0, value)
+        #pass
+
+    def dequeue(self):
+        if len(self.storage) >= 1:
+            return self.storage.pop(-1)
+
+
+
 import time
 
 start_time = time.time()
@@ -13,10 +37,41 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+
+## Not sure if this is allowed or not, but it's only 1 second runtime
+# for name_1 in names_1:
+#     if name_1 in names_2:
+#         duplicates.append(name_1)
+
+
+
+## Basically the same thing, 1.08 seconds
+queue = Queue()
+
+for name1 in names_1:
+    queue.enqueue(name1)
+for name2 in names_2:
+    if name2 in queue.storage:
+        duplicates.append(name2)
+
+
+## This is not a good solution
+# i=0
+# for name1 in names_1:
+#     queue.enqueue(name1)
+
+# while i <= len(queue):
+#     for name2 in names_2:
+#         if name2 == queue.storage[i]:
+#             duplicates.append(name2)
+#             i+=1
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +81,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
